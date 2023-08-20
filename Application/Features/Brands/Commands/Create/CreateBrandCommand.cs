93cbb6b4b0f9fa3f418e4b773e6afc768c1cc0d6
Mazhar Selvitopi <selvitopi_25@hotmail.com>
2023-08-20 15:56:41 +0300

    5. video completed. Added MediatR

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    // her comment'in bir handler'i vardır. MediatR
+    public class CreateBrandCommand:IRequest<CreatedBrandResponse>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+        {
+            public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+                createdBrandResponse.Id = new Guid();
+                createdBrandResponse.Name = request.Name;
+
+                return null;
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand)
+        {
+            
+        }
+    }
+}
